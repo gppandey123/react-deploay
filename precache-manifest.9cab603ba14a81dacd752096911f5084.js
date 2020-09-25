3bbe6b4700e6ba01c33ed4cb1dc67431314821b7
gppandey123 <gppandey9919@gmail.com>
2020-09-25 13:41:18 +0530

    Updates

---
+++ precache-manifest.9cab603ba14a81dacd752096911f5084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cfd63b5961c7f9d42b13efc014a556b",
+    "url": "/gppandey123.github.io/react-deploay/index.html"
+  },
+  {
+    "revision": "314c5e877edb9c175892",
+    "url": "/gppandey123.github.io/react-deploay/static/css/main.4a61b3a2.chunk.css"
+  },
+  {
+    "revision": "6f3774b405726cb5917b",
+    "url": "/gppandey123.github.io/react-deploay/static/js/2.59c2b2f0.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/gppandey123.github.io/react-deploay/static/js/2.59c2b2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "314c5e877edb9c175892",
+    "url": "/gppandey123.github.io/react-deploay/static/js/main.658c436e.chunk.js"
+  },
+  {
+    "revision": "b8455e67012430193442",
+    "url": "/gppandey123.github.io/react-deploay/static/js/runtime-main.68738d73.js"
+  }
+]);
